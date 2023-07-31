8a3eed7e8dd0be293b8604c3abfdc26502253263
DhavalGV <dhaval.halari@globalvoxinc.com>
2023-07-31 10:54:28 +0530

    Next Auth Changes

---
+++ src/pages/_app.js
@@ -0,0 +1,12 @@
+
+import { SessionProvider } from "next-auth/react";
+
+export default function App({ Component, pageProps }) {
+  const { session, ...restPageProps } = pageProps;
+
+  return (
+    <SessionProvider session={session}>
+      <Component {...restPageProps} />
+    </SessionProvider>
+  );
+}
---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,4 @@
+export default function Dashboard() {
+  return <h1>Some super secret dashboard</h1>;
+}
+Dashboard.auth = true;
